fe697bc010e0d897f66580cf6a967e442f513b3d
prost333 <dimabolshoy333@yandex.by>
2023-04-17 11:55:49 +0300

    ty

--- src/Garage.java
+++ src/Garage.java
@@ -1,14 +1,3 @@
 public class Garage <T extends Vehicle>{
-    T transport;
-    public Garage(T transport){
-        this.transport=transport;
-    }
 
-    public T getTransport() {
-        return transport;
-    }
-
-    public void setTransport(T transport) {
-        this.transport = transport;
-    }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,6 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scnr=new Scanner(System.in);
-        System.out.println("Введите имя машины: ");
-        String nameCar1=scnr.nextLine();
-        System.out.println("Введите имя мотоцикла: ");
-        String namebike1=scnr.nextLine();
-        Car car1=new Car(nameCar1);
-        Moto bike1=new Moto(namebike1);
-        Garage<Car>carGarage=new Garage<>(new Car(nameCar1));
-        Garage<Moto>motoGarage= new Garage<>(new Moto(namebike1));
-        System.out.println("name car: "+carGarage.getTransport().getName());
-        System.out.println("Name bike: "+motoGarage.getTransport().getName());
+
     }
 }
